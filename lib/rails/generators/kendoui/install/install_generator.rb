9070633c9c08be3b5cbe2249f27f7f43c819f085
Brandon Satrom <bsatrom+gh@gmail.com>
2012-02-26 12:35:28 -0600

    fixing file paths in generator

--- lib/rails/generators/kendoui/install/install_generator.rb
+++ lib/rails/generators/kendoui/install/install_generator.rb
@@ -1,13 +1,13 @@
 require 'rails'
 require File.expand_path('../../../../../../lib/kendoui-rails/version', __FILE__)
 
-module KendoUI
+module Kendoui
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
 
       desc "This generator installs KendoUI #{Kendoui::Rails::KENDOUI_VERSION}"
       class_option :ui, :type => :boolean, :default => false, :desc => "Include jQueryUI"
-      source_root File.expand_path('../../../../../vendor/assets/javascripts/kendo', __FILE__)
+      source_root File.expand_path('../../../../../../vendor/assets/javascripts/kendo', __FILE__)
 
       def copy_kendoui
         say_status("copying", "Kendo UI (#{Kendoui::Rails::KENDOUI_VERSION})", :green)
@@ -17,7 +17,7 @@ module KendoUI
       #Add option to copy over another Style (Metro, etc)
       def copy_kendoui_styles
         say_status("copying", "Kendo UI (#{Jquery::Rails::KENDOUI_VERSION}) default stylesheets", :green)
-        source_root File.expand_path('../../../../../vendor/assets/stylesheets/kendo', __FILE__)
+        source_root File.expand_path('../../../../../../vendor/assets/stylesheets/kendo', __FILE__)
 
         copy_file "kendo.common.min.css", "public/stylesheets/kendo.common.min.css"
         copy_file "kendo.default.min.css", "public/stylesheets/kendo.default.min.css"
